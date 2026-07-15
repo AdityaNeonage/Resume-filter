dc6fa8af449e54e192dd15affb119a6ecc5db702
Aditya <poddaraditya2005@gmail.com>
2026-07-15 14:46:42 +0530

    first commit

---
+++ app2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="RESUME BUILDER APP",
+    page_icon="👍🏻",
+    layout="centered"
+)
+
+st.header("RESUME BUILDER APP BY GEMINI 3.5 TO YOU CV")
